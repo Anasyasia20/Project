fa146f5b75699e8612f80419fe675a91479acd43
Anasyasia20 <pistikwasdied@gmail.com>
2022-10-07 16:46:13 +0300

    м

--- variant1.py
+++ variant1.py
@@ -1,9 +1,9 @@
 print("Hello,world!!!")
 
-spisok = [3, 8, 5, 9, 1] #Последовательность с размерностью 5
-for j in range(1, len(spisok)): #Этот цикл отвечает за количество обходов ( их будет 5)
-    for i in range(1, len(spisok)): #Этот код делает один обход по последовательности
-        if spisok[i-1] > spisok[i]:
-            spisok[i-1], spisok[i] = spisok[i], spisok[i-1]
+spisok = [3, 8, 5, 9, 1] #Последовательность с длиной 5
+for j in range(0, len(spisok)): #цикл отвечает за количество обходов
+    for i in range(0, len(spisok)-1): #проход по парам последовательности (кроме последнего элемента, так как он уже макс)
+        if spisok[i] > spisok[i+1]:
+            spisok[i], spisok[i+1] = spisok[i+1], spisok[i]
 print(spisok)
-
+#длина списка = 5. но нумерация чисел начинается с 0, 5 позиции у нас нет, поэтому ставим -1
